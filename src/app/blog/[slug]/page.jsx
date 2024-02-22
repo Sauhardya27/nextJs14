f8a6caeca75892fa1d16f6321fbfb7bc68a38fc9
Sauhardya27 <sauhardyachakraborty2643@gmail.com>
2024-02-22 18:01:13 +0530

    Blog post page updated

--- src/app/blog/[slug]/page.jsx
+++ src/app/blog/[slug]/page.jsx
@@ -1,26 +1,42 @@
 import styles from "./singlePost.module.css"
 import Image from "next/image"
+import PostUser from "@/components/postUser/postUser"
+import { Suspense } from "react"
 
-const SinglePostPage = () => {
+const getData = async (slug) => {
+  const res = await fetch(`https://jsonplaceholder.typicode.com/posts/${slug}`)
+
+  if (!res.ok) {
+    throw new Error("Something went wrong")
+  }
+
+  return res.json()
+}
+
+const SinglePostPage = async ({ params }) => {
+
+  const { slug } = params;
+
+  const post = await getData(slug);
+
   return (
     <div className={styles.container}>
       <div className={styles.imgContainer}>
         <Image src="https://images.pexels.com/photos/9561829/pexels-photo-9561829.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" alt="" fill className={styles.img} />
       </div>
       <div className={styles.textContainer}>
-        <h1 className={styles.title}>Title</h1>
+        <h1 className={styles.title}>{post.title}</h1>
         <div className={styles.detail}>
           <Image src="https://images.pexels.com/photos/9561829/pexels-photo-9561829.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" alt="" width={50} height={50} className={styles.avatar} />
-          <div className={styles.detailText}>
-            <span className={styles.detailTitle}>Author</span>
-            <span className={styles.detailValue}>Terry Jefferson</span>
-          </div>
+          <Suspense fallback={<div>Loading...</div>}>
+            <PostUser userId={post.userId} />
+          </Suspense>
           <div className={styles.detailText}>
             <span className={styles.detailTitle}>Published</span>
             <span className={styles.detailValue}>20.02.24</span>
           </div>
         </div>
-        <div className={styles.content}>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Velit aut quod voluptas cupiditate. Quam eius maxime atque autem velit consequatur harum sequi ipsa, magnam veritatis hic asperiores nulla voluptas illum.</div>
+        <div className={styles.content}>{post.body}</div>
       </div>
     </div>
   )
--- src/app/blog/page.jsx
+++ src/app/blog/page.jsx
@@ -1,21 +1,27 @@
 import styles from "./blog.module.css"
 import PostCard from "@/components/postCard/postCard"
 
-const BlogPage = () => {
+const getData = async () => {
+  const res = await fetch("https://jsonplaceholder.typicode.com/posts", {next:{revalidate:3600}})
+
+  if(!res.ok){
+    throw new Error("Something went wrong")
+  }
+
+  return res.json()
+}
+
+const BlogPage = async () => {
+
+  const posts = await getData()
   return (
     <div className={styles.container}>
-      <div className={styles.posts}>
-        <PostCard />
+    {posts.map((post) => (
+      <div className={styles.posts} key={post.id}>
+        <PostCard post={post}/>
       </div>
-      <div className={styles.posts}>
-        <PostCard />
-      </div>
-      <div className={styles.posts}>
-        <PostCard />
-      </div>
-      <div className={styles.posts}>
-        <PostCard />
-      </div>
+    ))}
+      
     </div>
   )
 }
