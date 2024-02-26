de03fea40b68cd3bfa85b4996932c48550ca5e0d
Sauhardya27 <sauhardyachakraborty2643@gmail.com>
2024-02-26 23:53:59 +0530

    Error resolved, database is remained to be connected

--- src/app/blog/[slug]/page.jsx
+++ src/app/blog/[slug]/page.jsx
@@ -2,22 +2,22 @@ import styles from "./singlePost.module.css"
 import Image from "next/image"
 import PostUser from "@/components/postUser/postUser"
 import { Suspense } from "react"
+import { getPost } from "@/lib/data";
 
-const getData = async (slug) => {
-  const res = await fetch(`https://jsonplaceholder.typicode.com/posts/${slug}`)
+// const getData = async (slug) => {
+//   const res = await fetch(`https://jsonplaceholder.typicode.com/posts/${slug}`)
 
-  if (!res.ok) {
-    throw new Error("Something went wrong")
-  }
+//   if (!res.ok) {
+//     throw new Error("Something went wrong")
+//   }
 
-  return res.json()
-}
+//   return res.json()
+// }
 
 const SinglePostPage = async ({ params }) => {
-
   const { slug } = params;
-
-  const post = await getData(slug);
+  
+  const post = await getPost(slug);
 
   return (
     <div className={styles.container}>
@@ -25,18 +25,20 @@ const SinglePostPage = async ({ params }) => {
         <Image src="https://images.pexels.com/photos/9561829/pexels-photo-9561829.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" alt="" fill className={styles.img} />
       </div>
       <div className={styles.textContainer}>
-        <h1 className={styles.title}>{post.title}</h1>
+        <h1 className={styles.title}>{post?.title}</h1>
         <div className={styles.detail}>
           <Image src="https://images.pexels.com/photos/9561829/pexels-photo-9561829.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" alt="" width={50} height={50} className={styles.avatar} />
-          <Suspense fallback={<div>Loading...</div>}>
-            <PostUser userId={post.userId} />
-          </Suspense>
+          {post && (
+            <Suspense fallback={<div>Loading...</div>}>
+              <PostUser userId={post.userId} />
+            </Suspense>
+          )}
           <div className={styles.detailText}>
             <span className={styles.detailTitle}>Published</span>
             <span className={styles.detailValue}>20.02.24</span>
           </div>
         </div>
-        <div className={styles.content}>{post.body}</div>
+        <div className={styles.content}>{post?.body}</div>
       </div>
     </div>
   )
--- src/lib/utlis.js
+++ src/lib/utlis.js
@@ -5,11 +5,11 @@ const connection = {}
 export const connectToDb = async () => {
 	try {
 		if(connection.isConnected){
-			console.log("Using existing connection")
-			return
+			console.log("Using existing connection");
+			return;
 		}
-		const db = await mongoose.connect(process.env.MONGO)
-		connection.isConnected = db.connections[0].readyState
+		const db = await mongoose.connect(process.env.MONGO);
+		connection.isConnected = db.connections[0].readyState;
 	} catch (error) {
 		console.log(error)
 		throw new Error(error)
