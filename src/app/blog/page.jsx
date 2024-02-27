e9352837be57fdef1cbcd6aa1fde328c5d4c1cdf
Sauhardya27 <sauhardyachakraborty2643@gmail.com>
2024-02-27 20:54:11 +0530

    API Route Testing

--- src/app/blog/page.jsx
+++ src/app/blog/page.jsx
@@ -2,19 +2,19 @@ import styles from "./blog.module.css"
 import PostCard from "@/components/postCard/postCard"
 import { getPosts } from "@/lib/data";
 
-// const getData = async () => {
-//   const res = await fetch("https://jsonplaceholder.typicode.com/posts", {next:{revalidate: 3600}})
+const getData = async () => {
+  const res = await fetch("http://localhost:3000/api/blog", {next:{revalidate: 3600}})
 
-//   if (!res.ok) {
-//     throw new Error("Something went wrong")
-//   }
+  if (!res.ok) {
+    throw new Error("Something went wrong")
+  }
 
-//   return res.json()
-// }
+  return res.json()
+}
 
 const BlogPage = async () => {
 
-  const posts = await getPosts()
+  const posts = await getData()
   return (
     <div className={styles.container}>
       {posts.map((post) => (
