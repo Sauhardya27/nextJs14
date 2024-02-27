129b804d9a06f5bf5226a5b6adfb15879cfd12aa
Sauhardya27 <sauhardyachakraborty2643@gmail.com>
2024-02-27 11:45:51 +0530

    Connected to MongoDB database and successfully fetched data

--- src/app/blog/[slug]/page.jsx
+++ src/app/blog/[slug]/page.jsx
@@ -21,13 +21,12 @@ const SinglePostPage = async ({ params }) => {
 
   return (
     <div className={styles.container}>
-      <div className={styles.imgContainer}>
-        <Image src="https://images.pexels.com/photos/9561829/pexels-photo-9561829.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" alt="" fill className={styles.img} />
-      </div>
+      {post.img && (<div className={styles.imgContainer}>
+        <Image src={post.img} alt="" fill className={styles.img} />
+      </div>)}
       <div className={styles.textContainer}>
-        <h1 className={styles.title}>{post?.title}</h1>
+        <h1 className={styles.title}>{post.title}</h1>
         <div className={styles.detail}>
-          <Image src="https://images.pexels.com/photos/9561829/pexels-photo-9561829.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" alt="" width={50} height={50} className={styles.avatar} />
           {post && (
             <Suspense fallback={<div>Loading...</div>}>
               <PostUser userId={post.userId} />
@@ -35,10 +34,10 @@ const SinglePostPage = async ({ params }) => {
           )}
           <div className={styles.detailText}>
             <span className={styles.detailTitle}>Published</span>
-            <span className={styles.detailValue}>20.02.24</span>
+            <span className={styles.detailValue}>{post.createdAt.toString().slice(4, 16)}</span>
           </div>
         </div>
-        <div className={styles.content}>{post?.body}</div>
+        <div className={styles.content}>{post.desc}</div>
       </div>
     </div>
   )
--- src/components/postUser/postUser.jsx
+++ src/components/postUser/postUser.jsx
@@ -1,5 +1,6 @@
 import styles from "./postUser.module.css"
 import { getUser } from "@/lib/data";
+import Image from "next/image";
 
 // const getData = async (userId) => {
 // 	const res = await fetch(`https://jsonplaceholder.typicode.com/users/${userId}`, {cache:"no-store"})
@@ -16,8 +17,11 @@ const PostUser = async ({ userId }) => {
 	const user = await getUser(userId)
 	return (
 		<div className={styles.container}>
-			<span className={styles.title}>Author</span>
-			<span className={styles.username}>{user.username}</span>
+			<Image src={user.img ? user.img : "/noavatar.png"} alt="" width={50} height={50} className={styles.avatar} />
+			<div className={styles.texts}>
+				<span className={styles.title}>Author</span>
+				<span className={styles.username}>{user.username}</span>
+			</div>
 		</div>
 	)
 }
